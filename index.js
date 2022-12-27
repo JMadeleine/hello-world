31ce4c202c462f241e83777ef205c46ad84d8f34
JMadeleine <79150069+JMadeleine@users.noreply.github.com>
2022-12-27 12:52:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+alert("Hallo Moritz!");
+// Variable definieren (Kommentar = strg k, strg c)
+const button = document.getElementById("button1");
+// Variable abrufen:
+console.log(button)
+
+button.onclick=() => {
+    alert("Hallo!")
+}
